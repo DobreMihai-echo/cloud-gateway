2778ff7859a03b8969f646b4b01a64a4c7658412
Dobre Mihai <dobremihai97@yahoo.com>
2023-05-20 21:36:15 +0300

    Created Jenkins File, Docker and Kubernetes

---
+++ src/main/java/com/apigateway/apigateway/controller/FallbackController.java
@@ -0,0 +1,24 @@
+package com.apigateway.apigateway.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/usersServiceFallBack")
+    public String userServiceFallback() {
+        return "User service is down!";
+    }
+
+    @GetMapping("/eventsServiceFallBack")
+    public String eventsServiceFallback() {
+        return "Events service is down!";
+    }
+
+    @GetMapping("/notificationServiceFallBack")
+    public String notificationServiceFallback() {
+        return "Notification service is down!";
+    }
+}
