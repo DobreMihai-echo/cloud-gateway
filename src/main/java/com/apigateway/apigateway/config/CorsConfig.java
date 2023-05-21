b4dd57f81aff602d201af40c3fe877cf06bacf0d
Dobre Mihai <dobremihai97@yahoo.com>
2023-05-21 23:55:56 +0300

    Jenkins Pipeline

---
+++ src/main/java/com/apigateway/apigateway/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.apigateway.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
